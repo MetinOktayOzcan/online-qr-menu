a984cb319ba30d351cf28f8fd040b616e1e680c9
MetinOktayOzcan <metinoktayozcan@gmail.com>
2026-01-13 00:21:45 +0300

    Veritabanı bağlantısı sağlandı

--- core/connection.php
+++ core/connection.php
@@ -1,6 +1,13 @@
 <?php
-define('DBUSER', "root");
-define('DBPASS', "");
-define('DBNAME', "online-qr-menu");
-define('DBHOST', "localhost");
+$db_server ="localhost";
+$db_user = "root";
+$db_pass="";
+$db_name="online-qr-menu";
+$conn="";
+
+try{
+$conn =mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+}catch(mysqli_sql_exception){
+    echo "Veritabanı Hatası :(";
+}
 ?>
