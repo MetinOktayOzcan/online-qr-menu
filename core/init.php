a9ba8758ccb463d3d77ed6464cd62bd4582a1cc0
MetinOktayOzcan <metinoktayozcan@gmail.com>
2026-01-20 20:56:54 +0300

    SEO dostu url yapısına geçildi

---
+++ core/init.php
@@ -0,0 +1,7 @@
+<?php
+// session_start();
+require_once __DIR__ . '/connection.php';
+require_once __DIR__ . '/functions.php';
+
+define('ROOT','http://localhost/online-qr-menu');
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once "core/init.php"; 
+
+function url($key=null){
+    $arr = explode("/",trim($_GET['url'] ?? "home","/"));
+    if (!is_numeric($key)) {
+        return  $arr;
+    }
+    return $arr[$key] ?? "";
+    
+}
+$file = "pages/" . url(0) . ".php";
+if (file_exists($file)) {
+    require $file;
+}else{
+    require '404.php';
+}
+?>
